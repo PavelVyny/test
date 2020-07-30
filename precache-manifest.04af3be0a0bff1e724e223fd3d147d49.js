969e71ab662eafaa209be3049d7a5206d657e32d
Pavel Vynychenko <pavel.vynych@gmail.com>
2020-07-30 20:59:22 +0300

    Updates

---
+++ precache-manifest.04af3be0a0bff1e724e223fd3d147d49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e779b5d9d9bffe6d47be9cb5f5eec54d",
+    "url": "/PavelVyny/test/index.html"
+  },
+  {
+    "revision": "8dc71bbc7ccb926d7d88",
+    "url": "/PavelVyny/test/static/css/main.29910e67.chunk.css"
+  },
+  {
+    "revision": "0b78f47f7a28b70911a7",
+    "url": "/PavelVyny/test/static/js/2.8e159031.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PavelVyny/test/static/js/2.8e159031.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dc71bbc7ccb926d7d88",
+    "url": "/PavelVyny/test/static/js/main.c9fb4478.chunk.js"
+  },
+  {
+    "revision": "8afa3d7c634966d82cca",
+    "url": "/PavelVyny/test/static/js/runtime-main.ad8c3594.js"
+  }
+]);
